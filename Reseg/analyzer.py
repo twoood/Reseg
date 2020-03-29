e7ab79300fb58cf51488827a41a9e38c737e10d4
Tom Wood <tom.wood@adtran.com>
2020-03-29 13:34:13 -0500

    update analyzer

--- Reseg/analyzer.py
+++ Reseg/analyzer.py
@@ -52,30 +52,56 @@ def identify_hosts(packets):
     
     return host_dict
 
+def threshold_enforce(traffic_dict, ip_totals, threshold):
+    suggested_seg={}
+
+    for name, h_object in traffic_dict.items():
+        for ip, packet in h_object.items():
+            if (packet/ip_totals[name]) > (threshold/100):
+                suggested_seg[name] = ip  
+
+    for host, client in suggested_seg.items():
+        print(str(host)+ " suggested to segement with " + str(client))
+
+
+
+
 def main():
     layout = [      
             [sg.Text('Please enter path of PCAP file and any subnets')],      
-            [sg.Text('PCAP', size=(15, 1)), sg.InputText('./testv1.pcapng')],      
-            [sg.Text('subnets', size=(15, 1)), sg.InputText('10.10.10.x, 10.10.20.x')],            
+            [sg.Text('PCAP', size=(15, 1)), sg.InputText('./testv1.pcapng')],   
+            [sg.Text('Threshold', size=(15, 1)), sg.InputText('70%(default)')],    
+            [sg.Text('Subnets', size=(15, 1)), sg.InputText('10.10.10.x, 10.10.20.x')],            
             [sg.Submit(), sg.Cancel()]      
             ]      
 
     window = sg.Window('Traffic Analysis').Layout(layout)         
     button, values = window.Read()
     window.close() 
-    logging.debug(button, values[0], values[1])
+    logging.debug(button, values[0], values[1], values[2])
 
     pack = rdpcap(values[0])
-    if values[1] != '10.10.10.x, 10.10.20.x':
+    if values[2] != '10.10.10.x, 10.10.20.x':
         print("Not currently implemented")
+    
+    if values[1] != '70%(default)':
+        threshold = values[1]
+    else:
+        threshold = 70
 
     analysis_info = identify_hosts(pack)
 
+    ip_totals={}
     for name, h_object in analysis_info.items():
+        total=0
         print(str(name))
         for ip, packet in h_object.items():
             print(str(ip) + " : " + str(packet))
-        print('\n')  
+            total+=packet
+        ip_totals[name]=total
+        print('Total Traffic :  ' + str(total) + '\n')  
+
+    threshold_enforce(analysis_info, ip_totals, threshold)
 
 
 if __name__ == "__main__":
