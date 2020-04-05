203d9a2afcf085c6d25eccd27c5c2475afb8d7b3
Tom Wood <tom.wood@adtran.com>
2020-04-04 23:31:56 -0500

    added functionality to get payload size

--- Reseg/analyzer.py
+++ Reseg/analyzer.py
@@ -23,6 +23,7 @@ def identify_hosts(packets):
             if packet.type == 2048:
                 if packet['IP'].src not in unique_hosts:
                     unique_hosts.append(packet['IP'].src)
+                    
                     host_information[packet['IP'].src] = HostObject(packet["IP"].src)
                 else:
                     port = 0
@@ -34,30 +35,32 @@ def identify_hosts(packets):
                         port = "icmp"
                     else:
                         logging.warning("Unknown protocol")
-                    host_information[packet['IP'].src].add_egress(packet['IP'].dst, port)
+                    host_information[packet['IP'].src].add_egress(packet['IP'].dst, port, len(packet.payload))
 
             elif packet.type == 34525:
                 logging.debug("IPv6 packet")
             elif packet.type == 2054:
                 logging.debug("ARP packet")
+            elif packet.type == 36864:
+                logging.debug("Loopback packet")
+            elif packet.type == 24578:
+                logging.debug("DEC MOP Remote Console packet")
             else:
                 logging.warning(str(packet.type) + " not recognized")
         except Exception as e:
-            # pdb.set_trace()
             print("Exception " + str(e))
             
     host_dict = {}
     for host_ip, host_info in host_information.items():
         host_dict[host_ip] = host_info.get_host_traffic()
-    
     return host_dict
 
-def threshold_enforce(traffic_dict, ip_totals, threshold):
+def threshold_enforce(traffic_dict, ip_totals, payloads, threshold, slider):
     suggested_seg={}
 
     for name, h_object in traffic_dict.items():
         for ip, packet in h_object.items():
-            if (packet/ip_totals[name]) > (int(threshold)/100):
+            if (packet['packets']/ip_totals[name]) > (int(threshold)/100):
                 suggested_seg[name] = ip  
 
     
@@ -72,45 +75,51 @@ def threshold_enforce(traffic_dict, ip_totals, threshold):
 def main():
     layout = [      
             [sg.Text('Please enter path of PCAP and any subnets')],      
-            [sg.Text('PCAP', size=(15, 1)), sg.InputText('./pcap_files')],   
-            [sg.Text('Threshold', size=(15, 1)), sg.InputText('70%(default)')],    
-            [sg.Text('Subnets', size=(15, 1)), sg.InputText('10.10.10.x, 10.10.20.x')],            
+            [sg.Text('PCAP', size=(15, 1)), sg.InputText('./segmented_pcap')],   
+            [sg.Text('Overall Threshold (%)', size=(15, 1)), sg.InputText('50 (default)')], 
+            [sg.Text('Adjust the Slider to Favor Data over Total Packets')],
+            [sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=75)],         
             [sg.Submit(), sg.Cancel()]      
             ]      
 
     window = sg.Window('Traffic Analysis').Layout(layout)         
     button, values = window.Read()
     window.close() 
-    logging.debug(button, values[0], values[1], values[2])
+    logging.debug(button, values[0], values[1])
 
     for filename in os.listdir(values[0]):
         if filename.endswith(".pcapng") or filename.endswith(".pcap"): 
             pack = rdpcap(os.path.join(values[0],filename))
+            logging.debug("Pcap file found:" + str(filename))
         else:
             continue
 
-    
-    if values[2] != '10.10.10.x, 10.10.20.x':
-        print("Not currently implemented")
-    
-    if values[1] != '70%(default)':
+    threshold = 50
+    slider = 75
+    if values[1] != '50 (default)':
         threshold = values[1]
-    else:
-        threshold = 70
+    
+    if values[2] != 75:
+        slider = values[2]
 
     analysis_info = identify_hosts(pack)
 
     ip_totals={}
+    payload_totals={}
     for name, h_object in analysis_info.items():
         total=0
+        total_payload=0
         print(str(name))
         for ip, packet in h_object.items():
             print(str(ip) + " : " + str(packet))
-            total+=packet
+            total+=packet['packets']
+            total_payload+=packet['payload_size']
         ip_totals[name]=total
-        print('Total Traffic :  ' + str(total) + '\n')  
+        payload_totals[name]=total_payload
+        print('Total Traffic :  ' + str(total))
+        print('Total Payload :  ' + str(total_payload) + '\n')
     suggestions={}
-    suggestions=threshold_enforce(analysis_info, ip_totals, threshold)
+    suggestions=threshold_enforce(analysis_info, ip_totals, payload_totals, threshold, slider)
     suggest="With a threshold of " + str(threshold) + "%\n\n"
     for host, client in suggestions.items():
        suggest+=(str(host)+ " suggested to resegement with " + str(client)+ "\n")
--- Reseg/host.py
+++ Reseg/host.py
@@ -1,19 +1,41 @@
+'''
+Object to store the ip objects
+
+HostObject
+
+ip - ip_name (string)
+hosts - list
+    [{
+        "ip": ip_name (string),
+        "payload": payload_amount(int)
+        "egress":
+        [
+            "port": port_number(int)
+            "packets": packets_sent(int)
+        ]
+
+    }]
+'''
 class HostObject:
     def __init__(self, ip):
         self.ip = ip
         self.hosts = []
-        self.total = 1
+        self.total = 0
+        
 
     def get_ip(self):
         return self.ip
     
-    def add_egress(self, egress_ip, egress_port):
+    def add_egress(self, egress_ip, egress_port, payload_amount):
         self.total+=1
-        host = self.find_host(egress_ip, egress_port)
-        if host != "No match":
-            self.add_packet_info(host, egress_port)
+        host_found = self.find_host(egress_ip, egress_port)
+        if host_found != "No match":
+            self.add_packet_info(host_found, payload_amount, egress_port)
         else:
-            self.hosts.append({"ip":egress_ip, "egress":[{"port":egress_port, "packets":1}]})
+            self.hosts.append({
+                "ip":egress_ip, 
+                "payload":payload_amount,
+                "egress":[{"port":egress_port, "packets":1}]})
 
     def find_host(self, egress_ip, egress_port):
         for host in self.hosts:
@@ -21,24 +43,30 @@ class HostObject:
                 return host
         return "No match"
 
-    def add_packet_info(self, host, e_port):
+    def add_packet_info(self, found_host, payload_amount, e_port):
         match = False
-        for port in host["egress"]:
+        found_host["payload"]+=payload_amount
+        for port in found_host["egress"]:
             if port["port"] == e_port:
                 port["packets"] += 1
                 match = True
         if not match:
-            host["egress"].append({"port":e_port, "packets":1})
+            found_host["egress"].append({"port":e_port, "packets":1})
 
     def get_total(self):
         return self.total
 
+    def get_payload(self):
+        return self.payload_total
+
     def get_host_traffic(self):
         host_values = {}
-        packets = 0
+        packs = 0
+        pay_load = 0
         for host in self.hosts:
+            pay_load+=host["payload"]
             for packet_vals in host["egress"]:
-                packets+=packet_vals["packets"]
-            host_values[host["ip"]] = packets
+                packs+=packet_vals["packets"]
+            host_values[host["ip"]] = {"packets": packs, "payload_size": pay_load}
         return host_values
 
